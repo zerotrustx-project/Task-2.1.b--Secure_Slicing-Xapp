32e5a6b8859f9ec1a8ab0e063344b9fbcb39a704
Charles Ueltschey <chaseuelt@gmail.com>
2024-05-13 16:10:04 -0500

    cleanup build directory

--- testing/build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/charles/Task-2.1.b--Secure_Slicing-Xapp/testing/build/_deps/googletest-src/googletest
-# Build directory: /home/charles/Task-2.1.b--Secure_Slicing-Xapp/testing/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- testing/build/hello_test[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test( HelloTest.BasicAssertions /home/charles/Task-2.1.b--Secure_Slicing-Xapp/testing/build/hello_test [==[--gtest_filter=HelloTest.BasicAssertions]==] --gtest_also_run_disabled_tests)
-set_tests_properties( HelloTest.BasicAssertions PROPERTIES WORKING_DIRECTORY /home/charles/Task-2.1.b--Secure_Slicing-Xapp/testing/build SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set( hello_test_TESTS HelloTest.BasicAssertions)
